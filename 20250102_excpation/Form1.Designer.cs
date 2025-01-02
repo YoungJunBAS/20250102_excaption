45abccbdd3e569b060503ee2246bfbc256cd8337
YoungJunBAS <ayjayj0218@gmail.com>
2025-01-02 13:41:33 +0900

    Add project files.

---
+++ 20250102_excpation/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace _20250102_excpation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox.Location = new System.Drawing.Point(12, 49);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.Size = new System.Drawing.Size(776, 376);
+            this.textBox.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ 20250102_excpation/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace _20250102_excpation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string FilePath = @"C:\\Users\\dudwn\\Desktop\\스마트팩토리SW과정\\4주차\\input.txt";
+
+            string[] FileContentArray;
+
+
+            ReadTxtFile(FilePath, out FileContentArray);
+
+
+
+        }
+
+        public void ReadTxtFile(string FilePath, out string[] InputTxTArray)
+        {
+            string content = File.ReadAllText(FilePath);
+            int lineCount = 0;
+
+            try
+            {
+                string[] TxtLines = File.ReadAllLines(FilePath);
+                lineCount = TxtLines.Length;
+            }
+
+            catch (IOException ex)
+            {
+                textBox.Text = "오류발생!";
+            }
+            finally
+            {
+                InputTxTArray = new string[lineCount];
+            }
+        }
+    }
+}
